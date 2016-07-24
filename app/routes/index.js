7b0379ca2d2bd7e82800a5ea6bc8a974a8f8071b
Bram de Greve <bram.degreve@bramz.net>
2016-07-24 19:40:34 +0200

    home is puzzle lock

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
 import Layout from 'pages/layout'
-import Home from 'pages/home'
+import PuzzleLock from 'pages/puzzle-lock'
 
 // not a function this time.
 export default (
   <Route path='/' component={Layout}>
-    <IndexRoute component={Home} />
+    <IndexRoute component={PuzzleLock} />
   </Route>
 )

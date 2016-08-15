d38e4be389fa9bedeff9afa5e6e9247e6759a25a
Bram de Greve <bram.degreve@bramz.net>
2016-08-15 13:54:19 +0200

    timer on holy grail

--- frontend/app/pages/holy-grail/index.js
+++ frontend/app/pages/holy-grail/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
+import Timer from 'containers/timer'
 import Video from 'components/video'
 
 import style from './style.less'
 
 export default () =>
-  <div className={style.holy}>
-    <Video />
+  <div className={style.wrapper}>
+    <div className={style.timer}>
+      <Timer />
+    </div>
+    <div className={style.main}>
+      <div className={style.child}>
+        <Video />
+      </div>
+    </div>
   </div>

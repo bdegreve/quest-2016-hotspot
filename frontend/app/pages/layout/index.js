19c043a9a131c371ef1c3601150d679d544455a7
Bram de Greve <bram.degreve@bramz.net>
2016-08-15 14:02:30 +0200

    timer on holy grail page

--- frontend/app/pages/layout/index.js
+++ frontend/app/pages/layout/index.js
@@ -3,13 +3,25 @@ import { connect } from 'react-redux'
 
 import PuzzleLock from 'pages/puzzle-lock'
 import HolyGrail from 'pages/holy-grail'
+import Timer from 'containers/timer'
 import Loading from 'components/loading'
+
+import style from './style.less'
 
 const View = ({loaded, unlocked}) => {
   if (!loaded) {
     return <Loading />
   }
-  return unlocked ? <HolyGrail /> : <PuzzleLock />
+  return <div className={style.wrapper}>
+    <div className={style.timer}>
+      <Timer />
+    </div>
+    <div className={style.main}>
+      <div className={style.child}>
+        {!unlocked ? <HolyGrail /> : <PuzzleLock />}
+      </div>
+    </div>
+  </div>
 }
 
 const mapStateToProps = (state) => ({
--- frontend/app/pages/puzzle-lock/index.js
+++ frontend/app/pages/puzzle-lock/index.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux'
 import Dimensions from 'react-dimensions'
 
 import Puzzle from 'containers/15-puzzle'
-import Timer from 'containers/timer'
 import PlayerSelect from 'containers/player-select'
 
 import style from './style.less'
@@ -28,9 +27,6 @@ const View = ({players}) =>
   <div className={style.wrapper}>
     <div className={style.select}>
       <PlayerSelect />
-    </div>
-    <div className={style.timer}>
-      <Timer />
     </div>
     <div className={style.puzzle}>
       <div className={style.child}>

acfea2f4de83b87f381e104990f0dc2d2ae706d4
Bram de Greve <bram.degreve@bramz.net>
2016-08-17 09:51:56 +0200

    ksa de graal reclame

--- frontend/app/components/ranking/index.js
+++ frontend/app/components/ranking/index.js
@@ -8,20 +8,21 @@ export default ({timer, players}) => {
   const started = timer.started
   const player = players.players[players.selected]
 
+  // reversed sorting.
   const stoppedPlayers = players.players.filter((p) => p.stopped)
   stoppedPlayers.sort((a, b) => {
     if (a.stopped < b.stopped) {
-      return -1
+      return 1
     }
     if (b.stopped < a.stopped) {
-      return 1
+      return -1
     }
     return 0
   })
 
   return <div className={style.ranking}>
-    <h2>Dag rangschikking</h2>
-    <ol>
+    <h2>dag rangschikking</h2>
+    <ol reversed>
       {stoppedPlayers.map((p) =>
         <li key={p.name} className={p.name === player.name ? style.this : style.other}>
           {p.name} - {humanizeTime(p.stopped - started)}
--- frontend/app/pages/holy-grail/index.js
+++ frontend/app/pages/holy-grail/index.js
@@ -8,5 +8,12 @@ import style from './style.less'
 export default () =>
   <div className={style.wrapper}>
     <Video />
+    <p className={style.quote}>
+      Lorum Ipsum
+    </p>
     <Ranking />
+    <div className={style.ksadegraal}>
+      <img src={require('./ksadegraal.png')} />
+      <p>KSA De Graal zoekt nieuwe leiding. Meld je aan!</p>
+    </div>
   </div>

06daaf71364400045d0b99786ec2ad372f98b776
Bram de Greve <bram.degreve@bramz.net>
2018-12-26 20:28:13 +0100

    upgrading to webpack 4

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,80 +1,154 @@
-var path = require('path')
-var webpack = require('webpack')
-var autoprefixer = require('autoprefixer')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
-var StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+const autoprefixer = require('autoprefixer')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin')
 
-var DEBUG = process.env.NODE_ENV !== 'production'
+const DEBUG = process.env.NODE_ENV !== 'production'
+const HASH = !DEBUG ? '-[hash]' : ''
+const CHUNKHASH = !DEBUG ? '-[chunkhash]' : ''
 console.log('DEBUG:', DEBUG)
 
-module.exports = {
-  entry: {
-    main: [
-      'babel-polyfill',
-      './app/app.js'
-    ]
+const plugins = [
+  new webpack.LoaderOptionsPlugin({
+    debug: !!DEBUG,
+    minimize: !DEBUG
+  }),
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  }),
+  new MiniCssExtractPlugin({
+    filename: `[name]${CHUNKHASH}.css`,
+    chunkFilename: `[id]${CHUNKHASH}.css`
+  }),
+  new StaticSiteGeneratorPlugin({
+    entry: 'main',
+    paths: ['/'],
+    globals: {
+      // shimming 'window' as self, to make webpack-dev-server/client happy.
+      self: {
+        location: {},
+        postMessage: () => {},
+        addEventListener: () => {}
+      }
+    }
+  })
+]
+
+const cssLoaders = [
+  // always using MiniCssExtractPlugin.loader even in debug mode, because
+  // style-loader in combination with static-site-generator-webpack-plugin
+  // gives a "window is not defined" error.
+  MiniCssExtractPlugin.loader,
+  {
+    loader: 'css-loader',
+    options: {
+      // don't localize names by default, otherwise bootstrap get
+      // localized too. Instead explicitly :local(...) what needs to be.
+      // ?importLoaders=1 seems not required for now.
+      // https://css-tricks.com/css-modules-part-3-react/
+      // https://github.com/css-modules/css-modules
+      modules: 'global',
+      localIdentName: '[local]_[hash:base64:5]'
+    }
   },
-  debug: DEBUG,
+  {
+    loader: 'postcss-loader',
+    options: {
+      plugins: () => [
+        autoprefixer({
+          browsers: ['last 3 versions', '> 0.1%', 'android > 4']
+        })
+      ]
+    }
+  }
+]
+
+module.exports = {
+  mode: DEBUG ? 'development' : 'production',
+  entry: {
+    main: ['babel-polyfill', './app/app.js']
+  },
   output: {
-    path: path.resolve('dist'),
-    filename: '[name]-[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    filename: `[name]${CHUNKHASH}.js`,
+    chunkFilename: `[name]${CHUNKHASH}-chunk.js`,
     libraryTarget: 'umd',
-    pathinfo: DEBUG
+    pathinfo: !!DEBUG,
+    // errr, a "temp" hack for a "window is not defined" error we get in
+    // static-site-generator-webpack-plugin with webpack 4
+    // https://github.com/markdalgleish/static-site-generator-webpack-plugin/issues/130
+    globalObject: "typeof self !== 'undefined' ? self : this"
+
   },
   devtool: DEBUG ? '#eval-source-map' : '#source-map',
   module: {
-    preLoaders: [
+    rules: [
       {
         test: /\.jsx?$/,
-        loader: 'standard',
-        exclude: /(node_modules|bower_components)/
-      }
-    ],
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel',
         exclude: /(node_modules|bower_components)/,
-        query: {} // see .babelrc
+        enforce: 'pre',
+        use: 'standard-loader'
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: 'babel-loader' // see .babelrc
+      },
+      {
+        test: /\.css$/,
+        use: cssLoaders
       },
       {
         test: /\.less$/,
-        loader: ExtractTextPlugin.extract('style', 'css?localIdentName=[local]-[hash:base64:5]!postcss!less')
+        use: [
+          ...cssLoaders,
+          {
+            loader: 'less-loader',
+            options: {
+              // hack for less-loader 4.0 that now uses webpack's loaders
+              // by default, and thus can't cope with @import url(https://...)
+              // Specifying paths option forces it back to less' original
+              // loaders
+              paths: [
+                path.resolve(__dirname, './app'),
+                path.resolve(__dirname, 'node_modules')
+              ]
+            }
+          }
+        ]
       },
       {
         test: /\.(png|jpg|jpeg|eot|ttf|svg|woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file'
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: `[name]${HASH}.[ext]`
+            }
+          }
+        ]
       },
       {
         test: /\.(mp4|webm|ogv)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file'
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: `[name]${HASH}.[ext]`
+            }
+          }
+        ]
       }
     ]
   },
-  plugins: [
-    new ExtractTextPlugin('[name]-[hash].css'),
-    new StaticSiteGeneratorPlugin('main', ['/'])
-  ].concat(DEBUG ? [] : [
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-    })
-  ]),
+  plugins,
   resolve: {
-    root: path.resolve('./app'),
-    extensions: ['', '.js'],
-    modulesDirectories: [
+    modules: [
+      path.resolve(__dirname, 'app'),
+      __dirname,
       'node_modules',
       'web_modules' // because https://github.com/webpack/webpack-dev-server/issues/60
     ]
-  },
-  postcss: [
-    autoprefixer({
-      browsers: [
-        'last 2 versions',
-        '> 0.1%',
-        'android > 4']
-    })
-  ]
+  }
 }
